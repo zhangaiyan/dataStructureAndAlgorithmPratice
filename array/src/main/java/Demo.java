b871cdc6cc866a0a93bdc295799617c82913b2fd
zhangaiyan <727812934@qq.com>
2019-10-31 14:16:30 +0800

    tst demo

---
+++ array/src/main/java/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * @author zhangchaoyue
+ * @date 2019/10/31
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        System.out.println(array[0]);
+    }
+}
+
