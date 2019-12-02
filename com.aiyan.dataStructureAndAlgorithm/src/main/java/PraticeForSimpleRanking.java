c5c5da53e0ba3e48fc4291b28d4d453cc1cb0a79
zhangaiyan <727812934@qq.com>
2019-12-02 12:03:14 +0800

    选择排序及插入排序代码

--- com.aiyan.dataStructureAndAlgorithm/src/main/java/PraticeForSimpleRanking.java
+++ com.aiyan.dataStructureAndAlgorithm/src/main/java/PraticeForSimpleRanking.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * @author zhangchaoyue
  * @date 2019/11/29
@@ -11,13 +6,17 @@ public class PraticeForSimpleRanking {
 
     public static void main(String[] args) {
         int[] arr = generateRandom();
-        bubbleSort(arr);
+//        bubbleSort(arr);
+//        selectSort(arr);
+        insertionSort(arr);
         displayResult(arr);
 
     }
 
     /**
      * 冒泡排序
+     * 外层循环控制每一轮比较的次数，内存循环进行比较并交换
+     *
      * @param arr
      */
     public static void bubbleSort(int[] arr) {
@@ -30,18 +29,56 @@ public class PraticeForSimpleRanking {
         }
     }
 
-    public static void selectSort(List<Integer> list) {
+    /**
+     * 选择排序
+     * 逐个选择最小值放在最左端
+     *
+     * @param arr
+     */
+    public static void selectSort(int[] arr) {
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swap(arr, i, min);
+            }
+        }
     }
 
-    public static void insertionSort(List<Integer> list) {
-
+    /**
+     * 插入排序
+     * 临界值左边为已排序部分，右边为未排序部分
+     *
+     * @param arr
+     */
+    public static void insertionSort(int[] arr) {
+        //已排序部分
+        for (int i = 1; i < arr.length; i++) {
+            //标记了未排序部分最左端的数据
+            int temp = arr[i];
+            int j = i;
+            //将未排序部分逐个插入到已排序部分
+            while (j > 0 && temp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
     }
 
     private static void swap(int[] arr, int j) {
-        int t = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = t;
+        swap(arr, j + 1, j);
+    }
+
+    private static void swap(int[] arr, int i, int min) {
+        int t = arr[min];
+        arr[min] = arr[i];
+        arr[i] = t;
     }
 
     private static int[] generateRandom() {
